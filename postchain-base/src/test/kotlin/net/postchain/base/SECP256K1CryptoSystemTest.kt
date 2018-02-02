a738197bf149f8752f5008e1e40fb2b106935364
Kalle Rosenbaum <kalle@rosenbaum.se>
2018-02-02 19:12:15 +0100

    Fixed BigInteger usage to treat input as unsigned number.

--- postchain-base/src/test/kotlin/net/postchain/base/SECP256K1CryptoSystemTest.kt
+++ postchain-base/src/test/kotlin/net/postchain/base/SECP256K1CryptoSystemTest.kt
@@ -2,20 +2,26 @@
 
 package net.postchain.base
 
+import net.postchain.common.toHex
 import org.junit.Assert.*
 import org.junit.Test
+import java.util.Random
 
 class SECP256K1CryptoSystemTest {
-     @Test
+    @Test
     fun testSignVerify() {
-         val SUT = SECP256K1CryptoSystem()
-         val privKey = ByteArray(32, {1.toByte()})
-         val pubKey = secp256k1_derivePubKey(privKey)
-         val signer = SUT.makeSigner(pubKey, privKey)
-         val data = "Hello".toByteArray()
-         val signature = signer(data)
-         val verifier = SUT.makeVerifier()
-         assertTrue(verifier(data, signature))
-         assertFalse(verifier("Hell0".toByteArray(), signature))
-     }
+        val SUT = SECP256K1CryptoSystem()
+        val random = Random()
+        var privKey = ByteArray(32)
+        for (i in 0..39) {
+            random.nextBytes(privKey)
+            val pubKey = secp256k1_derivePubKey(privKey)
+            val signer = SUT.makeSigner(pubKey, privKey)
+            val data = "Hello".toByteArray()
+            val signature = signer(data)
+            val verifier = SUT.makeVerifier()
+            assertTrue("Positive test failed for privkey ${privKey.toHex()}", verifier(data, signature))
+            assertFalse("Negative test failed for privkey ${privKey.toHex()}", verifier("Hell0".toByteArray(), signature))
+        }
+    }
 }
